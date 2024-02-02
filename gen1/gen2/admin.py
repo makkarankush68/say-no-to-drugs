9b12b1963b54239a4d67b1b84d61cd29b5fcf989
chandanbir <chandanbirsingh1@gmail.com>
2024-02-02 18:01:45 +0530

    gesnfle

--- gen1/gen2/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import *
-# Register your models here.
-class UserDataAdmin(admin.ModelAdmin):
-  list_display=('name','email','password')
-class postsAdmin(admin.ModelAdmin):
-  list_display=('uploaderemail','uploader','location','time','file','desc','comments')
-admin.site.register(UserData,UserDataAdmin)
-admin.site.register(posts,postsAdmin)
